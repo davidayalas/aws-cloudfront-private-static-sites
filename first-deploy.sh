1d5f0155e65f9176dced4c131c1b20bd262f1e63
davidayalas <davixyz@gmail.com>
2020-10-22 08:16:54 +0200

    first-deploy

--- first-deploy.sh
+++ first-deploy.sh
@@ -1,5 +1,6 @@
 npm install serverless-s3-sync
 npm --prefix ./backend/login install ./backend/login
+sls deploy
 cfendpoint=`sls info --verbose | awk -F"[: ]+" '/CloudFrontDNS/{print $2}'`
 sed 's/cloudfrontendpoint/'"$cfendpoint"'/g' web/auth-sample.html > web/auth.html
 sls s3sync
